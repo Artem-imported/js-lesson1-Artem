70b6f948a1611a651abc171132d703e5b8aa5f91
Artem-imported <goreliiartemii261201@gmail.com>
2024-08-26 11:40:32 +0200

    1

---
+++ script.js
@@ -0,0 +1,21 @@
+const age = 12;
+console.log(age);
+
+const name = Artem;
+console.log(typeof name);
+
+const isStudent = true; 
+console.log(isStudent);
+
+
+const myString = "Ваш час обмежений, не витрачайте його, живучи чиїмось життям.";  // Вкажіть свою улюблену цитату
+console.log(myString);
+
+
+const myNumber = 42;
+myNumber += 10;
+console.log(myNumber);
+
+
+const myNull = null;
+console.log(myNull);
