459b96223f9fd9c77d591a4e9f9683089b07a73b
Artem-imported <goreliiartemii261201@gmail.com>
2024-08-30 10:53:08 +0200

    2

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
 const age = 12;
 console.log(age);
 
-const name = Artem;
-console.log(typeof name);
+const name = "Artem";
+console.log(name);
 
 const isStudent = true; 
 console.log(isStudent);
 
 
-const myString = "Ваш час обмежений, не витрачайте його, живучи чиїмось життям.";  // Вкажіть свою улюблену цитату
+const myString = "Ваш час обмежений, не витрачайте його, живучи чиїмось життям.";
 console.log(myString);
 
 
-const myNumber = 42;
+let myNumber = 42;
 myNumber += 10;
 console.log(myNumber);
 
 
 const myNull = null;
 console.log(myNull);
+
+let userName = prompt("Будь ласка, введіть своє ім'я:");
+alert("Привіт, " + userName + "!");
+
+let confirma = confirm("Ви хочете продовжити?");
+if (confirma) {
+    alert("Дякую за підтвердження!");
+} else {
+    alert("Дію відмінено!");
+}
+
+alert("Увага! Ця дія може бути небезпечною.");
+let userConfirm = confirm("Ви впевнені, що хочете продовжити?");
+if (userConfirm) {
+    alert("Дякую за підтвердження!");
+} else {
+    alert("Дію відмінено!");
+}
